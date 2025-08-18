48c01031aae79657a7c9baa39788b8779d6e6cfa
danhtran2mind <cprmmb99@gmail.com>
2025-08-18 09:40:43 +0700

    Change some codes

--- src/audioldm/inference/ldm.py
+++ src/audioldm/inference/ldm.py
@@ -3,11 +3,11 @@ import os
 import torch
 import numpy as np
 from tqdm import tqdm
-from audioldm.utils import default, instantiate_from_config, save_wave
-from audioldm.latent_diffusion.ddpm import DDPM
-from audioldm.variational_autoencoder.distributions import DiagonalGaussianDistribution
-from audioldm.latent_diffusion.util import noise_like
-from audioldm.latent_diffusion.ddim import DDIMSampler
+from .utils import default, instantiate_from_config, save_wave
+from .latent_diffusion.ddpm import DDPM
+from .variational_autoencoder.distributions import DiagonalGaussianDistribution
+from .latent_diffusion.util import noise_like
+from .latent_diffusion.ddim import DDIMSampler
 import os
 
 
--- src/audioldm/inference/pipeline.py
+++ src/audioldm/inference/pipeline.py
@@ -6,10 +6,10 @@ import torch
 from torch import autocast
 from tqdm import tqdm, trange
 
-from audioldm import LatentDiffusion, seed_everything
-from audioldm.utils import default_audioldm_config, get_duration, get_bit_depth, get_metadata, download_checkpoint
-from audioldm.audio import wav_to_fbank, TacotronSTFT, read_wav_file
-from audioldm.latent_diffusion.ddim import DDIMSampler
+from . import LatentDiffusion, seed_everything
+from .utils import default_audioldm_config, get_duration, get_bit_depth, get_metadata, download_checkpoint
+from .utilities.audio import wav_to_fbank, TacotronSTFT, read_wav_file
+from .latent_diffusion.ddim import DDIMSampler
 from einops import repeat
 import os
 
